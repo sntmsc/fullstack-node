8898c02fbb33fb93bfe7860d9dd9792656c47ae9
sntmsc <santiago.hamber@gmail.com>
2022-07-01 11:08:26 -0300

    heroku added

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+//app.use(morgan('tiny'))
+
+morgan.token('object', function(req,res){
+    return `${JSON.stringify(req.body)}`
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
+
+app.use(cors())
 
 let persons = [
     {
@@ -81,7 +92,7 @@ app.post('/api/persons', (request, response) =>{
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
